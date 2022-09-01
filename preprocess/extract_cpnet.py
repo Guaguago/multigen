2ee891dd7aced7518b1ead10282a6bdd3c27cafd
xuchen <chen.xu@geelink.cn>
2022-09-02 00:00:16 +0800

    ~

--- preprocess/extract_cpnet.py
+++ preprocess/extract_cpnet.py
@@ -1,5 +1,6 @@
 import configparser
 import json
+from tqdm import tqdm
 
 relation_mapping = dict()
 
@@ -40,37 +41,40 @@ def extract_english():
 
     only_english = []
     with open(config["paths"]["conceptnet"], encoding="utf8") as f:
-        for line in f.readlines():
-            ls = line.split('\t')
-            if ls[2].startswith('/c/en/') and ls[3].startswith('/c/en/'):
-                """
-                Some preprocessing:
-                    - Remove part-of-speech encoding.
-                    - Split("/")[-1] to trim the "/c/en/" and just get the entity name, convert all to 
-                    - Lowercase for uniformity.
-                """
-                rel = ls[1].split("/")[-1].lower()
-                head = del_pos(ls[2]).split("/")[-1].lower()
-                tail = del_pos(ls[3]).split("/")[-1].lower()
+        lines = f.readlines()
+        with tqdm(total=len(lines)) as pbar:
+            for line in lines:
+                ls = line.split('\t')
+                if ls[2].startswith('/c/en/') and ls[3].startswith('/c/en/'):
+                    """
+                    Some preprocessing:
+                        - Remove part-of-speech encoding.
+                        - Split("/")[-1] to trim the "/c/en/" and just get the entity name, convert all to 
+                        - Lowercase for uniformity.
+                    """
+                    rel = ls[1].split("/")[-1].lower()
+                    head = del_pos(ls[2]).split("/")[-1].lower()
+                    tail = del_pos(ls[3]).split("/")[-1].lower()
 
-                if not head.replace("_", "").replace("-", "").isalpha():
-                    continue
+                    if not head.replace("_", "").replace("-", "").isalpha():
+                        continue
 
-                if not tail.replace("_", "").replace("-", "").isalpha():
-                    continue
+                    if not tail.replace("_", "").replace("-", "").isalpha():
+                        continue
 
-                if rel not in relation_mapping:
-                    continue
-                rel = relation_mapping[rel]
-                if rel.startswith("*"):
-                    rel = rel[1:]
-                    tmp = head
-                    head = tail
-                    tail = tmp
+                    if rel not in relation_mapping:
+                        continue
+                    rel = relation_mapping[rel]
+                    if rel.startswith("*"):
+                        rel = rel[1:]
+                        tmp = head
+                        head = tail
+                        tail = tmp
 
-                data = json.loads(ls[4])
+                    data = json.loads(ls[4])
 
-                only_english.append("\t".join([rel, head, tail, str(data["weight"])]))
+                    only_english.append("\t".join([rel, head, tail, str(data["weight"])]))
+                    pbar.update(1)
 
     with open(config["paths"]["conceptnet_en"], "w", encoding="utf8") as f:
         f.write("\n".join(only_english))
